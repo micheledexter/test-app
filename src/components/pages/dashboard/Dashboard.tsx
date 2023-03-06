2ed2b7849f7a74c9cdf95edfb60e594f619f81b1
Michele <micheleannedexter@gmail.com>
2023-03-06 17:54:52 -0600

    Refactor dashboard to not include unnecessary props

--- src/components/pages/dashboard/Dashboard.tsx
+++ src/components/pages/dashboard/Dashboard.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import clsx from "clsx";
 import { Theme } from "@mui/material";
 import { createStyles, makeStyles } from "@mui/styles";
 
-interface Props {
-  className?: string;
-  children?: React.ReactNode;
-}
-
-export const Dashboard = ({ className, children }: Props) => {
+export const Dashboard = () => {
   const classes = useStyles();
 
-  return <div className={clsx(className, classes.root)}>{children}</div>;
+  return (
+    <div>
+      <div>test</div>
+    </div>
+  );
 };
 
 const useStyles = makeStyles((theme: Theme) =>
